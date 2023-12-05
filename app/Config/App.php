cb821182b9d7b53a26912e676e65dd2631248761
Your Name <you@example.com>
2023-12-05 20:44:19 +0700

    edit base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,9 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = BASE;
+    // public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://kmpsmart.co.id/';
+
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
