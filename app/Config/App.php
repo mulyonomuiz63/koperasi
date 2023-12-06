d3229bbdf2b224a24ce25b13b5f9407d1e13b044
Your Name <you@example.com>
2023-12-06 21:02:02 +0700

    url

--- app/Config/App.php
+++ app/Config/App.php
@@ -18,8 +18,8 @@ class App extends BaseConfig
      *    http://example.com/
      */
     // public string $baseURL = 'http://localhost:8080/';
-    // public string $baseURL = 'https://kmpsmart.co.id/';
-    public $baseURL = BASE;
+    public string $baseURL = 'https://kmpsmart.co.id/';
+    // public $baseURL = BASE;
 
 
     /**
