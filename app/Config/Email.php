baf6e0faf0b88d9857d14a93be968b774eeb8d8e
Your Name <you@example.com>
2023-12-07 19:43:46 +0700

    url

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -48,7 +48,7 @@ class Email extends BaseConfig
      * @var string
      */
     // Enter your email id from where you send email
-    public $SMTPUser = 'noreplay@kmpsmart.co.id';
+    public $SMTPUser = 'no-replay@kmpsmart.co.id';
 
     /**
      * SMTP Password
